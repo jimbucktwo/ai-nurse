e5ed31589dcbccf68019c5a954e98f3fec70b0a0
Hao Phan <rinnphan01@gmail.com>
2025-03-22 14:42:55 -0700

    Update daisyUI

--- ai-nurse/src/app/page.tsx
+++ ai-nurse/src/app/page.tsx
@@ -1,17 +1,19 @@
-import Image from "next/image";
+// import Image from "next/image";
+import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">AI Nurse</h1>
-      <p className="mt-4 text-lg">Your virtual healthcare assistant</p>
-      <Image
-        src="/ai-nurse.png"
-        alt="AI Nurse"
-        width={300}
-        height={300}
-        className="rounded-full mt-6"
-      />
-    </main>
+    // <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //   <h1 className="text-4xl font-bold">Nursai</h1>
+    //   <p className="mt-4 text-lg">Your virtual healthcare assistant</p>
+    //   <Image
+    //     src="/ai-nurse.png"
+    //     alt="AI Nurse"
+    //     width={300}
+    //     height={300}
+    //     className="rounded-full mt-6"
+    //   />
+    // </main>
+    <Navbar/>
   );
 }
