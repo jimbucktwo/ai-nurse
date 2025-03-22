6bda91690563d6088b298ce80a16c7b924efb649
Ajay Bhawariya <47675480+AJAYBHAWARIYA@users.noreply.github.com>
2025-03-22 15:55:18 -0700

    basic signing functionality

--- nextjs-fastapi/app/page.tsx
+++ nextjs-fastapi/app/page.tsx
@@ -1,16 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import Navbar from "../Components/NavBar";
+import Home from "./Home/page";
 
-export default function Home() {
+export default function appPage() {
   return (
-    <>
-      <Navbar />
-      <div>
-        Hello
-      </div>
-    </>
-    
-  
+    <Home/>
   )
 }
