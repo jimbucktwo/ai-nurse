99f6fe8027b89b8137a0384c18841c0b7e8c2401
jimbucktwo <92623786+jimbucktwo@users.noreply.github.com>
2025-03-22 18:16:19 -0700

    updates

--- nextjs-fastapi/app/page.tsx
+++ nextjs-fastapi/app/page.tsx
@@ -3,11 +3,13 @@ import Home from "./Home/page";
 import { useAuth } from "@clerk/nextjs";
 import LandingPage from "./Landing/page";
 import { useEffect, useState } from "react";
+import io from "Socket.IO-client"
+let socket
 
 export default function AppPage() {
   const { isSignedIn } = useAuth(); // Call useAuth as a function
   const [loading, setLoading] = useState(true);
-
+  useEffect(() => socketInitializer(), [])
   useEffect(() => {
     if (isSignedIn !== undefined) {
       setLoading(false);
