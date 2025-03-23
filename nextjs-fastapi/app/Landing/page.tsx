1d4dff02c5f0c4eca099bb13a8311e49f24070a4
Ajay Bhawariya <47675480+AJAYBHAWARIYA@users.noreply.github.com>
2025-03-22 19:10:17 -0700

    landing page info added

--- nextjs-fastapi/app/Landing/page.tsx
+++ nextjs-fastapi/app/Landing/page.tsx
@@ -1,6 +1,38 @@
+import { SignIn, SignInButton, SignUp, SignUpButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Router from "next/router";
 
 export default function LandingPage(){
+
     return(
-        <>This is landing page</>
+        <>
+            <div className="hero min-h-screen" style={{backgroundImage: `url('./nurse.jpg')`}}>
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-center">
+                    <div className="max-w-lg">
+                        <h1 className="text-5xl font-bold p-2">Hello There!</h1>
+                        <h4 className="text-xl font-semibold">Are you also tired of waiting in line at the hospital? <br/>Even for minor medical advice</h4>
+                        <p className="py-6">
+                            Nursai is here to help.
+                        </p>
+                        <SignInButton className="btn w-sm btn-outline btn-primary" />
+                    </div>
+                </div>
+            </div>
+            {/* <div className="card lg:card-side bg-base-100 shadow-sm">
+                <figure>
+                    <img
+                        src="./nurse.jpg"
+                        alt="Nurse" />
+                </figure>
+                <div className="card-body">
+                    <h2 className="card-title">Introduction</h2>
+                    <p>Don't have an account yet! Create one and get started.</p>
+                    <div className="card-actions justify-end">
+                        <SignUpButton className="btn btn-outline btn-primary" />
+                    </div>
+                </div>
+            </div> */}
+        </>
     )
 }
